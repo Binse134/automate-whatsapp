c0197641faaebe5586336e540261226a4f4bbcc7
Binse134 <144788776+Binse134@users.noreply.github.com>
2025-01-19 19:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def reply():
         msg = res.message("Hi, thanks for contacting *The Red Velvet*.\nYou can choose from one of the options below: "
                     "\n\n*Type*\n\n 1️⃣ To *contact* us \n 2️⃣ To *order* snacks \n 3️⃣ To know our *working hours* \n 4️⃣ "
                     "To get our *address*")
-        msg.media("https://i.ibb.co/BPKnXVP/Red-Velvet-Cake-Waldorf-Astoria.jpg")
         users.insert_one({"number": number, "status": "main", "messages": []})
     elif user["status"] == "main":
         try:
