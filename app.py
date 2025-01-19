3f179deda7c958a2ce6593878aad5da09fe76629
Binse134 <144788776+Binse134@users.noreply.github.com>
2025-01-19 19:00:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 from datetime import datetime
 
-# Replace YOUR_URL with your mongodb url
-cluster = MongoClient("mongodb+srv://binsealter:Binsealter@binsewhatsapp.cyoqz.mongodb.net/",tls=True, tlsAllowInvalidCertificates=True)
+cluster = MongoClient("mongodb+srv://binsealter:Binsealter@binsewhatsapp.cyoqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster["binsetest"]
 users = db["users"]
 orders = db["orders"]
